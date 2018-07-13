7412ae707a926ccf1de170adb595a6edf43e2682
yukitamura <yuki_tamura@diveintocode.jp>
2018-07-13 19:25:55 +0900

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: '自分のHerokuアプリのドメイン' }
+  config.action_mailer.default_url_options = { host: 'pacific-springs-48054' }
    ActionMailer::Base.delivery_method = :smtp
    ActionMailer::Base.smtp_settings = {
    user_name: ENV['SENDGRID_USERNAME'],
