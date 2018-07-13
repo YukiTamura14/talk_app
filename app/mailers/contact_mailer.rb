283782e060ac387b3df0b025bdf1d5ad32b4cae2
yukitamura <yuki_tamura@diveintocode.jp>
2018-07-13 19:54:01 +0900

    init

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
-    mail to: "yktmr@ruru.be", subject: "お問い合わせの確認メール"
+    mail to: "yktmr@sute.jp", subject: "お問い合わせの確認メール"
   end
 end
